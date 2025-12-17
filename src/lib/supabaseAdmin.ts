e064a6c77dba79d343ab09a404821acca4e5fe0e
virusmindra <tsykhotskyi@icloud.com>
2025-12-17 05:15:09 +0000

    chore: connect supabase

--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = process.env.SUPABASE_URL!;
-const service = process.env.SUPABASE_SERVICE_ROLE_KEY!;
-
-export const supabaseAdmin = createClient(url, service, {
-  auth: { persistSession: false },
-});
+export const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  { auth: { persistSession: false } }
+);
