5ef8aa825aea5cfd9cbac3404b53f64df5f2f245
virusmindra <tsykhotskyi@icloud.com>
2026-01-23 04:15:47 +0000

    Goals: save button + goal suggestion + formatting

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,11 +1,11 @@
-import { getServerSession } from "next-auth";
+// src/server/auth.ts
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/server/auth-options";
 
 export async function getSession() {
   return getServerSession(authOptions);
 }
 
-/** Достаём userId (тот, что ты прокидываешь в callback session) */
 export async function requireUserId() {
   const session = await getServerSession(authOptions);
   const userId = (session?.user as any)?.id as string | undefined;
